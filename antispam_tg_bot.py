870792d2267eb2f9b020b91d1fa30599efde732a
Miroslav Kungurov <56649199+tttdddnet@users.noreply.github.com>
2023-05-17 11:16:05 +0300

    Create antispam_tg_bot.py

---
+++ antispam_tg_bot.py
@@ -0,0 +1,17 @@
+import telebot
+import re
+
+TOKEN = ""
+GROUP_ID = 
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: "abcdef.com" in message.text)
+def handle_message(message):
+    chat_id = message.chat.id
+    message_id = message.message_id
+
+    bot.delete_message(chat_id, message_id)
+    #bot.send_message(chat_id, "Сообщение удалено")
+                
+bot.infinity_polling(timeout=10, long_polling_timeout = 5)
